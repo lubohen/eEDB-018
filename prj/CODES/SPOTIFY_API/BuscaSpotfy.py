972dc33e0ef6008a46be98cfc8ccd6a1e554dc53
lubohen <lbhenrique.infonegocios@gmail.com>
2023-10-17 03:01:02 -0300

    USP-UPG-20231017

---
+++ prj/CODES/SPOTIFY_API/BuscaSpotfy.py
@@ -0,0 +1,38 @@
+import requests
+
+CLIENT_ID = 'd4880e347df145ec95382a22b3305276'
+CLIENT_SECRET = '94491d60b1a345199d2ecdabcbe2e935'
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+})
+
+# convert the response to JSON
+auth_response_data = auth_response.json()
+
+# save the access token
+access_token = auth_response_data['access_token']
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+BASE_URL = 'https://api.spotify.com/v1/'
+track_name = "Bulldog Down in Sunny Tennessee"
+artist_name = "Charlie Poole"
+
+# make the API request
+r = requests.get(BASE_URL + 'search?q=track:' + track_name + '+artist:' + artist_name + '&type=track', headers=headers)
+
+# get the first track from the results
+track = r.json()['tracks']['items'][0]
+track_id = track['id']
+
+r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
+audio_features = r.json()
+
+for feature, value in audio_features.items():
+    print(f"{feature}: {value}")
